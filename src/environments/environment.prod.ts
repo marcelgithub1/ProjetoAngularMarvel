50d4a65ca66552f4508905b655f9a377e5bb6ab6
Marcelo Augusto <marcelgithub1@gmail.com>
2022-02-08 04:14:37 -0300

    chaves
    exclusão da public e privada

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   url_marvel: 'https://gateway.marvel.com/v1/public/',
-  private_key: '15ea64433f39c3813b18dea76b8279b88aaadd1c',
-  public_key: '7b7a2d5af657f35476d6321b223163de'
+  private_key: '',
+  public_key: ''
 };
